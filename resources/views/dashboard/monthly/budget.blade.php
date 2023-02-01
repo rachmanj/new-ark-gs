f50c0acd7028384c0ba0d0f2b34be2235679bbb0
rachmanj <rachmanj@gmail.com>
2023-02-01 10:20:32 +0800

    minor update on monthly dashb budget

--- resources/views/dashboard/monthly/budget.blade.php
+++ resources/views/dashboard/monthly/budget.blade.php
@@ -21,10 +21,10 @@
                   {{ $histories->where('project_code', $project)->where('gs_type', 'po_sent')->first() ? number_format($histories->where('project_code', $project)->where('gs_type', 'po_sent')->first()->amount / 1000, 2) : '' }}
                 </td>
                 <td class="text-right">
-                  {{ $plant_budget->where('project_code', $project)->first() ? number_format($plant_budget->where('project_code', $project)->first()->amount / 1000, 2) : '' }}
+                  {{ $plant_budget->where('project_code', $project)->first() ? number_format($plant_budget->where('project_code', $project)->sum('amount') / 1000, 2) : '' }}
                 </td>
                 <td class="text-right">
-                  {{ $histories->where('project_code', $project)->where('gs_type', 'po_sent')->first() && $plant_budget->where('project_code', $project)->first() ? number_format($histories->where('project_code', $project)->where('gs_type', 'po_sent')->first()->amount / $plant_budget->where('project_code', $project)->first()->amount * 100, 2) : '' }}
+                  {{ $histories->where('project_code', $project)->where('gs_type', 'po_sent')->first() && $plant_budget->where('project_code', $project)->first() ? number_format($histories->where('project_code', $project)->where('gs_type', 'po_sent')->first()->amount / $plant_budget->where('project_code', $project)->sum('amount') * 100, 2) : '' }}
                 </td>
               </tr>
           @endforeach
